5625f9a4557e84d787ccb4fbf29ed7fc9bf8b2e6
unknown <tongnguyennhattien140805@gmail.com>
2026-02-27 09:13:54 +0700

    feat: setup mysql

--- src/main/java/vn/hoidanit/laptopshop/HelloWorld.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloWorld.java
@@ -7,6 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
     @GetMapping("/")
     public String index() {
-        return "Hello World From Nhat Tien with love";
+        return "Hello World From Nhat Tien with love hehe";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "User page hello guys";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Admin page hello guys";
+    }
+
 }
