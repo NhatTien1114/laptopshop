a514b4ab01e5afe89953a5b77832b7318ba7083d
unknown <tongnguyennhattien140805@gmail.com>
2026-03-16 08:25:36 +0700

    ui: implement admin page

--- src/main/java/vn/hoidanit/laptopshop/domain/OrderDetail.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/OrderDetail.java
@@ -1,11 +1,13 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class OrderDetail {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
